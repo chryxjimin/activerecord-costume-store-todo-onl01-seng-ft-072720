7369416d0a2dffeb58364760f1493106229ee0a2
Christie Yoon <cleex1999@gmail.com>
2020-09-01 21:47:28 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,7 +3,7 @@ class CostumeStore < ActiveRecord::Migration[4.2]
    def change
       create_table :costume_stores do |t|
           t.string :name
-          t.float :price
+          t.string :location
           t.string :size
           t.string :image_url
           t.timestamps
